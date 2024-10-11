dc54d032068d5bb1f89b201d4bfbc83b14b7f798
Am1d1v <dimadiman1adima@gmail.com>
2024-10-12 00:34:05 +0300

    Canvas configuration

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+
+    const canvas = document.querySelector('#canvas1');
+    const context = canvas.getContext('2d');
+    canvas.width = 900;
+    canvas.height = 900;
+
+});
