5cc5562b140abb320a22cd5d401ccca32bc6cc04
Am1d1v <dimadiman1adima@gmail.com>
2024-10-12 20:46:08 +0300

    Drawing Animated Shapes

--- main.js
+++ main.js
@@ -6,7 +6,18 @@ window.addEventListener('load', () => {
     canvas.height = window.innerHeight;
 
     const game = new Game(canvas, context);
-    console.log(game)
+    context.fillStyle = 'white';
+
+    // Animate(rerender) object
+    function animate(){
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        game.render();
+        requestAnimationFrame(animate);
+
+    };   
+    requestAnimationFrame(animate);
+
+
 });
 
 // State of the game & state of all game objects
@@ -17,13 +28,26 @@ class Game {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
 
+        this.x = 100;
+
         window.addEventListener('resize', (event) => {
             this.resize(event.currentTarget.innerWidth, event.currentTarget.innerHeight);
         });
     };
 
+    // Set canvas size
     resize(width, height){
         this.canvas.width = Math.floor(width);
         this.canvas.height = Math.floor(height);
-    }
-}
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.render();
+    };
+
+    // Draw objects on canvas
+    render(){
+        this.context.fillRect(this.x, 10, 100, 50);
+        this.x++;
+    };
+};
+
