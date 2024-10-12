334d5dba5c39d531b8e6530eb1a9e991c5b89674
Am1d1v <dimadiman1adima@gmail.com>
2024-10-13 00:57:32 +0300

    Game Grid

--- main.js
+++ main.js
@@ -30,10 +30,30 @@ class Game {
         // Snake
         this.snake = new Snake(this, 0, 0, 1, 1);
 
+        
+        // Size(Width & Height) of the grid
+        this.cellSize = 60;
+
+        // Quantity of columns on the screen
+        this.colums = this.width / this.cellSize;
+
+        // Quantity of rows on the screen
+        this.rows = this.height / this.cellSize;
+
+
         window.addEventListener('resize', (event) => {
             this.resize(event.currentTarget.innerWidth, event.currentTarget.innerHeight);
         });
     };
+
+    // Render game grid
+    drawGrid(){
+        for(let y = 0; y <= this.rows; y++){
+            for(let x = 0; x <= this.colums; x++){
+                this.context.strokeRect(x * this.cellSize, y * this.cellSize, this.cellSize, this.cellSize);
+            }
+        }
+    }
 
     // Set canvas size
     resize(width, height){
@@ -46,6 +66,7 @@ class Game {
 
     // Draw objects on canvas
     render(){
+        this.drawGrid();
         this.snake.draw();
         this.snake.update();
     };
