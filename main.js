9deca22d870779919c0a4af44b7f8eee7e5612e0
Am1d1v <dimadiman1adima@gmail.com>
2024-10-12 19:46:53 +0300

    Responsive Canvas

--- main.js
+++ main.js
@@ -2,7 +2,28 @@ window.addEventListener('load', () => {
 
     const canvas = document.querySelector('#canvas1');
     const context = canvas.getContext('2d');
-    canvas.width = 900;
-    canvas.height = 900;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
+    const game = new Game(canvas, context);
+    console.log(game)
 });
+
+// State of the game & state of all game objects
+class Game {
+    constructor(canvas, context){
+        this.canvas = canvas;
+        this.context = context;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+
+        window.addEventListener('resize', (event) => {
+            this.resize(event.currentTarget.innerWidth, event.currentTarget.innerHeight);
+        });
+    };
+
+    resize(width, height){
+        this.canvas.width = Math.floor(width);
+        this.canvas.height = Math.floor(height);
+    }
+}
