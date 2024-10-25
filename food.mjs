fec56892c6496f1887639c863e8918db7fb90484
Am1d1v <dimadiman1adima@gmail.com>
2024-10-25 13:37:30 +0300

    Animated Food

--- food.mjs
+++ food.mjs
@@ -6,23 +6,34 @@ class Food {
         this.x;
         this.y;
         this.reset();
-        this.image = document.querySelector('#berry');
+        this.image = document.querySelector('#mushroom');
+        this.spriteWidth = 200;
+        this.spriteHeight = 400;
+        this.reset();
+
+        // Current frame of X axis
+        this.frameX = 0;
+
+        // Max number of frames
+        this.maxFrames = 8;
     };
 
     // Spawn if collisioned with snake
     reset(){
         this.x = Math.floor(Math.random() * this.game.colums);
         this.y = Math.floor(Math.random() * (this.game.rows - 2) + 2);
+
+        this.frameX = 0;
     };
 
     draw(){
         //this.game.context.fillStyle = 'orange';
         //this.game.context.fillRect(this.x * this.game.cellSize, this.y * this.game.cellSize, this.game.cellSize, this.game.cellSize);
-        this.game.context.drawImage(this.image, this.x * this.game.cellSize, this.y * this.game.cellSize, this.game.cellSize, this.game.cellSize);
+        this.game.context.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x * this.game.cellSize, (this.y - 1) * this.game.cellSize, this.game.cellSize, this.game.cellSize * 2);
     }
 
     update(){
-
+        if(this.frameX < this.maxFrames) this.frameX++
     }
 };
 
